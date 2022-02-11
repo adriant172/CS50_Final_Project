a58dbc7b36df5807f821f96e6b509514d2d047c7
Matthew <adriant172@gmail.com>
2022-02-12 01:31:39 +0900

    commit all recent updates and changes

---
+++ static/scripts.js
@@ -0,0 +1,17 @@
+function username_api(){
+    var request = new XMLHttpRequest()
+    request.open('GET', '/get_user_info_api', true)
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            var balance_btn = document.getElementById("navbarDropdownMenuLink");
+            balance_btn.innerHTML = data.value;
+        } else {
+            console.log("Error!!!!!");
+        }
+    }
+
+    request.send()
+
+}
