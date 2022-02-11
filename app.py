9bba6f31a0f77bf2bcc50c2d6673001cc2620070
Matthew <adriant172@gmail.com>
2022-02-12 01:24:31 +0900

    commit all recent updates and changes

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
+import json
 from cs50 import SQL
 from sqlite3 import connect
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from support_functions import apology, login_required, recipes_lookup
+from support_functions import apology, login_required, get_current_user, sp_recipe_look_up, usd, recipe_price, single_recipe
 from flask_sqlalchemy import SQLAlchemy
 
 app =  Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filters
+app.jinja_env.filters["usd"] = usd
+app.jinja_env.filters["recipe_price"] = recipe_price
+
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -28,6 +34,9 @@ db = SQL("sqlite:///recipe_app.db")
 #     raise RuntimeError("API_ID not set")
 
 
+
+
+
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
@@ -38,9 +47,11 @@ def index():
         recipe_search = request.form.get("recipe_search")
         if not recipe_search:
             return apology("Error: Blank search request. Please input a search request")
-        results = recipes_lookup(recipe_search)
+        results = sp_recipe_look_up(recipe_search)
         if not results:
             return apology("Could not retrieve results")
+        # Calculate recipe price and convert it from cents to dollars 
+        # total_price = (results["pricePerServing"] * results["servings"]) / 100
         return render_template("search_results.html", results=results)
     return apology("Page Not available!")
 
@@ -57,7 +68,7 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["password_hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -104,10 +115,15 @@ def register():
     else:
         return render_template("register.html")
 
-@app.route("/view_recipe")
+@app.route("/view_recipe", methods=["GET", "POST"])
 def view_recipe():
-    if request.method == "GET":
-        return render_template("view_recipe.html")
+    if request.method == "POST":
+        recipe_id = request.form.get("recipe_id")
+        image_url = request.form.get("image_url")
+        recipe = single_recipe(recipe_id)
+        total_price = recipe_price(recipe)
+        return render_template("view_recipe.html", recipe=recipe, image_url=image_url, total_price=total_price)
+    return render_template("view_recipe.html")
 
 @app.route("/search_results")
 def search_results():
@@ -122,6 +138,23 @@ def suggestions():
 @app.route("/settings")
 def settings():
     if request.method == "GET":
-        return render_template("settings.html")
+        user_id = session["user_id"]
+        # Grab all cusine types from DB and pass them to the settings route
+        all_cuisine_types = []
+        rows = db.execute("SELECT * FROM cuisine_tags")
+        for row in rows:
+            all_cuisine_types.append(row["cuisine_type"])
+        user_prefs = db.execute("SELECT * FROM user_preferences WHERE user_id=?", user_id )
+        user_info = db.execute("SELECT points, daily_budget FROM users WHERE id=?", user_id)
+        return render_template("settings.html", all_cuisine_types=all_cuisine_types, user_prefs=user_prefs, user_info=user_info)
+
+@app.route("/get_user_info_api")
+def get_user_info_api():
+    user_id = session["user_id"]
+    user = get_current_user(user_id)
+    username = user["username"]
+    return {"value": username}
+
+
 
 
--- support_functions.py
+++ support_functions.py
@@ -2,9 +2,20 @@ import os
 import requests
 import json
 import re
+from cs50 import SQL
 from flask import render_template, session, redirect
 from functools import wraps
 
+db = SQL("sqlite:///recipe_app.db")
+
+
+
+def usd(value):
+    #Format value to usd
+    return f"${value:,.2f}"
+
+def recipe_price(recipe):
+    return float(recipe["pricePerServing"]) * float(recipe["servings"]) / 100
 
 
 def recipes_lookup(search_string):
@@ -13,7 +24,7 @@ def recipes_lookup(search_string):
         search_string.replace(" ", "%")
         file = open("api_creds.json")
         json_file = json.load(file)
-        api_id= json_file["APP_ID"]
+        api_id= json_file["API_ID"]
         api_key=json_file["API_KEY"]
         file.close()
         url = f"https://api.edamam.com/api/recipes/v2?type=public&q={search_string}&app_id={api_id}&app_key={api_key}"
@@ -42,6 +53,7 @@ def recipes_lookup(search_string):
         recipe["calories"] = item_info["calories"]
         recipe["total_time"] = item_info["totalTime"]
         recipe["servings"] = item_info["yield"]
+        recipe["cuisine/dish_type"] = (item_info["cuisineType"], item_info["dishType"])
         recipes.append(recipe)
 
     return recipes
@@ -73,3 +85,48 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def get_current_user(user_id):
+        user_info = {}
+        row = db.execute("SELECT * FROM users WHERE id=?", user_id)
+        user_info["username"] = row[0]["username"]
+        user_info["points"] = row[0]["points"]
+        user_info["daily_budget"] = row[0]["daily_budget"]
+        return user_info
+
+
+def single_recipe(recipe_id):
+    file = open("spoon_api_creds.json", 'r')
+    json_file = json.load(file)
+    api_key = json_file["apiKey"]
+    file.close()
+    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}"
+    response = requests.get(url)
+    recipe = json.loads(response.text)
+    return recipe
+
+def sp_recipe_look_up(search_string):
+    search_string.replace(" ", "%")
+    file = open("spoon_api_creds.json")
+    json_file = json.load(file)
+    api_key = json_file["apiKey"]
+    file.close()
+    get_ids_url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&query={search_string}"
+    get_ids_response = requests.get(get_ids_url)
+    json_data = json.loads(get_ids_response.text)
+    ids = []
+    for item in json_data["results"]:
+        ids.append(str(item["id"]))
+    if not ids:
+        return None
+    ids_string = ','.join(ids)
+    print(f"IDs= {ids_string}")
+    get_recipes_url = f"https://api.spoonacular.com/recipes/informationBulk?apiKey={api_key}&ids={ids_string}"
+    get_recipes_resp = requests.get(get_recipes_url)
+    recipes = json.loads(get_recipes_resp.text)
+
+    return recipes
+    
+
+
+
