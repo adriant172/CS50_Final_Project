3a8f2fb82f050bc433c6f0c1e297e636b7050af9
Matthew <adriant172@gmail.com>
2022-03-03 01:49:01 +0900

    updated registration page with validation logic

--- static/scripts.js
+++ static/scripts.js
@@ -14,11 +14,30 @@ function username_api(){
     request.send()
 }
 
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector("main").style.height = window.innerHeight + "px";
-})
+// var ready = (callback) => {
+//     if (document.readyState != "loading") callback();
+//     else document.addEventListener("DOMContentLoaded", callback);
+// }
+// ready(() => {
+//     document.querySelector("main").style.height = window.innerHeight + "px";
+// })
+
+// This function provided by bootstrap documentation
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
 
